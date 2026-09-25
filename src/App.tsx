3927c696b80ffa5f823a46b35e82b0cd1d475444
L´Studio Ana <lstudioana@gmail.com>
2026-09-25 11:07:20 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -477,11 +477,11 @@ export default function App() {
   for (let d = 1; d <= diasEnMes; d++) diasRejillaMini.push(new Date(añoMini, mesMini, d));
 
   const hoursList = [
-    '09:00', '09:30', '10:00', '10:30',
-    '11:00', '11:30', '12:00', '12:30',
-    '13:00', '13:30', '16:00', '16:30',
-    '17:00', '17:30', '18:00', '18:30',
-    '19:00', '19:30'
+    '10:00', '10:15', '11:00', '11:15',
+    '12:00', '12:15', '13:00', '13:15',
+    '14:00', '14:15', '15:00', '15:15',
+    '16:00', '16:15', '17:00', '17:15',
+    '18:00', '18:15', '19:00', '19:15',
   ];
 
   // Calcular el lunes de la semana de la fecha seleccionada para mostrar los 6 días reales (Lunes a Sábado)
